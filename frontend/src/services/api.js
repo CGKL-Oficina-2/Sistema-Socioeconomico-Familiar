7f33d3f2e3a269f4af2ad4f31bb0364ee82efc9e
Luccas Hessel <luccashessel@gmail.com>
2026-05-13 15:59:54 -0300

    feat: integração inicial do frontend com API

---
+++ frontend/src/services/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: '/api',
+  headers: { 'Content-Type': 'application/json' },
+})
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('ellp_token')
+      delete api.defaults.headers.common['Authorization']
+      if (window.location.pathname !== '/login') window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
